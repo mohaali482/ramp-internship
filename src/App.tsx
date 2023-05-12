9d79a7465a019ce6a267335cf2a68a50c30423a2
Mohammed Ali <mohaali482@gmail.com>
2023-05-12 08:46:47 +0300

    Solved Bug #5

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App() {
   const { data: employees, ...employeeUtils } = useEmployees()
   const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
   const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false) // TODO: remove this for Bug 5 - Part 1. Couldn't remove this becuase of callout #2
 
   const transactions = useMemo(
     () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
@@ -20,7 +20,7 @@ export function App() {
   )
 
   const loadAllTransactions = useCallback(async () => {
-    setIsLoading(true)
+    setIsLoading(true) // TODO: remove this for Bug 5 - Part 1. Couldn't remove this becuase of callout #2
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
@@ -51,7 +51,7 @@ export function App() {
         <hr className="RampBreak--l" />
 
         <InputSelect<Employee>
-          isLoading={isLoading}
+          isLoading={employeeUtils.loading}
           defaultValue={EMPTY_EMPLOYEE}
           items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
           label="Filter by employee"
